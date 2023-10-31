1bb3a407d50a546e2379c60a0bcf18a8fdef6081
ojedaelias <eli24ojeda@gmail.com>
2023-10-31 18:18:41 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,19 @@
-let titulo = prompt("Hola! ingrese el titulo que quiera para la pagina");
+// let titulo = prompt("Hola! ingrese el titulo que quiera para la pagina");
 
-document.getElementById("titulo").innerHTML =  titulo;
+// document.getElementById("titulo").innerHTML =  titulo;
+
+const searchIcon = document.getElementById("lupa-icon");
+const searchBar = document.getElementById("search-bar");
+
+let isMoved=false;
+searchIcon.addEventListener("click", function() {
+    searchBar.style.right = searchBar.style.right === "0px" ? "-400px" : "0px";
+    
+    if (!isMoved) {
+        searchIcon.style.transform = "translateX(-200px)";
+        isMoved = true;
+    } else {
+        searchIcon.style.transform = "translateX(0)";
+        isMoved = false;
+    }
+});
