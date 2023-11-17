2ea1695e7f7ea50042c5cc5cf7b44194cc90140d
ojdaelias <eli24ojeda@gmail.com>
2023-11-17 12:59:33 -0300

    update (boton carrito y animacion de img/confirm)

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 // let titulo = prompt("Hola! ingrese el titulo que quiera para la pagina");
 
 // document.getElementById("titulo").innerHTML =  titulo;
+// Buscador
 
 const searchIcon = document.getElementById("lupa-icon");
 const searchBar = document.getElementById("search-bar");
@@ -17,3 +18,39 @@ searchIcon.addEventListener("click", function() {
         isMoved = false;
     }
 });
+
+
+//------------>Carrito
+let carrito = [];
+
+function agregarAlCarrito(button){
+    const producto = button.parentElement;
+    const id = producto.dataset.id;
+    const nombre = producto.dataset.nombre;
+    const precio = parseFloat(producto.dataset.precio);
+    const imagen = producto.querySelector('img');
+    const productoSeleccionado = {id, nombre, precio};
+    const confirmacion = producto.querySelector('.confirmacion');
+    
+    carrito.push(productoSeleccionado);
+
+    
+    confirmacion.style.display = 'block';
+    imagen.style.filter = 'blur(3px)';
+
+    setTimeout(() => {
+        confirmacion.style.opacity = '1';
+        }, 100);
+
+    setTimeout(() => {
+        confirmacion.style.opacity = '0';
+        imagen.style.filter = 'blur(0px)';
+
+        setTimeout(() => {
+            confirmacion.style.display = 'none';
+        }, 500);
+        }, 1000);
+    
+    console.log(`¡${productoSeleccionado} ha sido agregado al carrito!`);
+}
+
